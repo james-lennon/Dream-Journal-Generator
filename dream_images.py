2a3b93050e57e59f0df0b76333e1acd06ab53cfb
James Lennon <jameslennon321@gmail.com>
2015-12-13 16:02:20 -0500

    Small adjustments

--- dream_images.py
+++ dream_images.py
@@ -6,6 +6,7 @@ from flickr_api import Photo, Walker
 FLICKR_USER_ID = '126377022@N07'
 API_KEY = "9fc869c68e7ddb1d6cb48f64a8044a78"
 API_SECRET = "f0b61a5aec4da80b"
+MAX_RESULTS = 20
 
 flickr_api.set_keys(API_KEY, API_SECRET)
 
@@ -18,10 +19,6 @@ def get_photo(keyword, surreal=False):
     num_results = len(w)
     if num_results == 0:
         return False
-    photo_num = random.randrange(min(num_results, 10))
+    photo_num = random.randrange(min(num_results, MAX_RESULTS))
     photo = w[photo_num:].next()
     return photo.getPhotoFile()
-
-
-if __name__ == '__main__':
-    print get_photo("dream")
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import content_render
 
 journal = content_render.DreamJournal()
 
-journal.generate_dreams(50, add_images=False)
+journal.generate_dreams(20, add_images=False)
 
 journal.render("out", pdf=False)
